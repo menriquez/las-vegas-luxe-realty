f4aa3ffbd7c0b591a14e2882986409bc4e051b82
mark enriquez <mark@webwarephpdevelopment.com>
2020-12-14 11:19:08 -0500

    removed private info

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ $email_subject = "Feedback form submitted";
 //recaptcha config
 $use_recaptcha = true;
 $publickey = "6LfbiiQUAAAAALGCA34UF3a4wvi9l67Z7ivSiEnH";
-$privatekey = "6LfbiiQUAAAAAHgnrQjKn0G2ETmF7t_0LY_Y_vjo";
+$privatekey = "";
 
 ?>                           
